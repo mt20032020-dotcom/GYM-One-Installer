67e7f030cbeb36d18dadfa103d21363057e2d71c
mt20032020-dotcom <mt2003.2020@gmail.com>
2026-07-11 20:13:14 -0500

    Create index.php

---
+++ iclock/cdata/iclock/getrequest/index.php
@@ -0,0 +1,14 @@
+<?php
+// Endpoint ADMS: /iclock/getrequest
+// El equipo consulta si hay comandos pendientes (ENROLL_BIO, DATA UPDATE, etc.)
+
+header('Content-Type: text/plain');
+
+$sn = $_GET['SN'] ?? '';
+$logFile = __DIR__ . '/../device_log.txt';
+
+file_put_contents($logFile, date('Y-m-d H:i:s') . " GETREQUEST SN={$sn}\n", FILE_APPEND);
+
+// TODO: aquí consultamos una tabla de "comandos pendientes" en la base de datos
+// Por ahora no hay comandos pendientes (respuesta vacía = OK sin acción)
+exit;
