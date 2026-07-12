615df06b2a395790468ff12a0c862baec91a762a
mt20032020-dotcom <mt2003.2020@gmail.com>
2026-07-12 03:29:48 -0500

    Update index.php

--- iclock/registry/index.php
+++ iclock/registry/index.php
@@ -2,10 +2,12 @@
 // Endpoint ADMS: /iclock/registry
 header('Content-Type: text/plain');
 
+$sn = $_GET['SN'] ?? '';
 $rawData = file_get_contents('php://input');
 $logFile = __DIR__ . '/../device_log.txt';
 
 file_put_contents($logFile, date('Y-m-d H:i:s') . " REGISTRY\n" . $rawData . "\n---\n", FILE_APPEND);
 
-echo "OK";
+$registryCode = strtoupper(substr(md5($sn . 'ADRENALINE'), 0, 10));
+echo "RegistryCode=$registryCode\r\n";
 exit;
